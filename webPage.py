38d0c60405a3ccaa2da06eadec5d880799e71090
m131601 <77653698+m131601@users.noreply.github.com>
2021-08-07 12:53:00 -0400

    update

--- webPage.py
+++ webPage.py
@@ -8,60 +8,26 @@
 # Tested OS:  This code was written and tested to work with Windows 10.
 
 
-import tkinter
-from tkinter import *
+import tkinter as tk
+from tkinter import simpledialog
+
+ROOT = tk.Tk()
+
+ROOT.withdraw()
+# the input dialog
+Inp_user = simpledialog.askstring(title="HTML",
+                                  prompt="What's your Name?:")
 
 
+print("Hello", Inp_user)
 
-
-class ParentWindow(Frame):
-    def __init__ (self, master):
-        Frame.__init__ (self)
-
-        self.master = master
-        self.master.resizable(width=True, height=True)
-        self.master.geometry('{}x{}'.format(700, 400))
-        self.master.title('Web Page Generator!')
-        self.master.config(bg='lightgray')
-
-        self.varFName = StringVar()
-        self.varLName = StringVar()
-
-        self.lblFName = Label(self.master,text='First Name: ', font=('Helvetica', 16), fg='black', bg='lightgray')
-        self.lblFName.grid(row=0, column=0,padx=(30,0), pady=(30,0))
-        
-        self.lblLName = Label(self.master,text='Last Name: ', font=('Helvetica', 16), fg='black', bg='lightgray')
-        self.lblLName.grid(row=1, column=0,padx=(30,0), pady=(30,0))
-
-        self.lblEmail = Label(self.master,text='Email: ', font=('Helvetica', 16), fg='black', bg='lightgray')
-        self.lblEmail.grid(row=2, column=0,padx=(30,0), pady=(30,0))
-
-        self.lblDisplay = Label(self.master,text='', font=('Helvetica', 16), fg='black', bg='lightgray')
-        self.lblDisplay.grid(row=3, column=1,padx=(30,0), pady=(30,0))
-       
-
-        self.txtFName = Entry(self.master,text=self.varFName, font =('Helvetica', 16), fg='black', bg='lightblue')
-        self.txtFName.grid(row=0, column=1,padx=(30,0), pady=(30,0))
-        
-        self.txtLName = Entry(self.master,text=self.varLName, font =('Helvetica', 16), fg='black', bg='lightblue')
-        self.txtLName.grid(row=1, column=1,padx=(30,0), pady=(30,0))
-
-        self.txtEmail = Entry(self.master,text=self.varLName, font =('Helvetica', 16), fg='black', bg='lightblue')
-        self.txtEmail.grid(row=2, column=1,padx=(30,0), pady=(30,0))
-
-        self.btnSubmit = Button(self.master, text='Submit', width=10, height=2, command=self.submit)
-        self.btnSubmit.grid(row=3, column=1,padx=(0,0), pady=(30,0), sticky=NE)
-
-        self.btnCancel = Button(self.master, text='Cancel', width=10, height=2, command=self.cancel)
-        self.btnCancel.grid(row=3, column=1,padx=(0,90), pady=(30,0), sticky=NE)
-
-    def submit(self):
+def submit(self):
         fn = self.varFName.get()
         ln = self.varLName.get()
         self.lblDisplay.config(text='Hello {} {}!'.format(fn,ln))
         
 
-    def cancel(self):
+def cancel(self):
         self.master.destroy()
 
     
@@ -69,7 +35,7 @@ class ParentWindow(Frame):
 
 import webbrowser
 
-f = open('sale.html','w')
+f = open('sale.html','a')
 
 message = """<html>
 <body><h1>Stay tuned for our amazing summer sale!</h1></body>
@@ -87,6 +53,4 @@ webbrowser.open_new_tab('sale.html')
 
 
 if __name__ == "__main__":
-    root = Tk()
-    App = ParentWindow(root)
-    root.mainloop()
+    ROOT.withdraw()
